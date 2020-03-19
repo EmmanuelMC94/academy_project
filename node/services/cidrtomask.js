4f972f39d57171977433f9a84230bc97872370fe
EmmanuelMC94 <emmanuel_m_c@hotmail.com>
2020-03-19 16:35:44 +0000

    sonar modification

--- node/services/cidrtomask.js
+++ node/services/cidrtomask.js
@@ -1,5 +1,26 @@
 export const cidrToMaskFunction = (value) => {
-  //console.log(value);
-  return value;
-
+//console.log(value);
+  
+ if(Number.isNaN(value)) {
+ return "Invalid" 
+          }
+           
+ if (value>=0 && value<=32) {
+    return createNetmaskAddr(value);
+   	        }
+      	else {
+return "Invalid"
+		}
+				}
+    function createNetmaskAddr(bitCount) {
+       var mask=[];
+       var i;
+       for(let i=0;i<4;i++){
+        var n=Math.min(bitCount,8);
+        mask.push(256-Math.pow(2,8-n));
+        bitCount -= n;
+            }
+        return mask.join('.');
+               	        				                                        
+          } 
 }
