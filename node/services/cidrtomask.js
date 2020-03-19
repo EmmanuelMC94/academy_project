0c5738e43bdf0381f2f3c4a79cd45bb3206cea9a
EmmanuelMC94 <emmanuel_m_c@hotmail.com>
2020-03-19 17:53:50 +0000

    changes in cid

--- node/services/cidrtomask.js
+++ node/services/cidrtomask.js
@@ -1,14 +1,4 @@
 export const cidrToMaskFunction = (value) => {
-//console.log(value);
-  
- if(Number.isNaN(value)) {
- return "Invalid" 
-          }
-           
- if (value>=0 && value<=32) {
-    return createNetmaskAddr(value);
-   	        }
-      	else {
-return "Invalid"
-		}
+  //console.log(value);
+  return value;
 }
